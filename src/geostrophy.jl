9a7c9e0ee6fae278f61d10a4c3e777e23f1ff986
jmbeckers <jm.beckers@ulg.ac.be>
2018-04-01 22:40:04 +0200

    Documentation

--- src/geostrophy.jl
+++ src/geostrophy.jl
@@ -1,24 +1,35 @@
 """
-Calculates geostrophic velocities
-works with one or two horizontal dimensions and additional (time) dimensions
-Dimensions are supposed to be ordered horizontal, vertical, other dimensions
+
+    velocity,ssh,fluxes=geostrophy(mask::BitArray,rhop,pmnin,xiin;dim::Integer=0,ssh=(),znomotion=0,fillin=true)
+	
+	
+
 
 
 # Input:
-* rhop: density anomaly array
-* pmnin: tuple of metrics as in divand, but should be in m 
-* xiin: tuple position of the grid points.
-* either provide level of no motion or ssh eta LEVEL IS INDEX NUMBER FOR THE MOMENT
-* dimensions of ssh must be the same as rhop in which vertical dimension has been taken out
-* If you force fillin=false, then you must have created the density array without missing values outside of this call
+* `mask` : Boolean array with true in water and false on land. 
+* `rhop` : density anomaly (rho-1025) array on the same grid as the mask. 
+* `pmnin`: tuple of metrics as in divand, but to get velocities in m/s the metrics need to be in per meters too.
+* `xiin`: tuple position of the grid points.
+* `dim` : optional paramter telling which index in the arrays corresponds to the vertical direction. By default 0 uses the last index
+* `ssh` : array as mask for which the vertical direction is taken out. Corresponds to sea surface height in meters. Default is no used but diagnosed
+* `znomotion` : index in the vertical direction where a level of no motion is assumed
+* `fillin` : Boolean telling if a filling of land points using water points at the same level is to be used. Default is yes. 
+
 
 
 # Output:
-* Velocity tuple components NORMAL and to the left of each coordinate line
-* eta : sea surface height deduced
-* fluxes: integrated velocities across sections.
+* `velocity` tuple of velocity components NORMAL and to the left of each coordinate line
+* `eta` : sea surface height deduced. If a ssh was provided in input it returs ssh but filled in on land.
+* `fluxes`: integrated velocities across sections in each horizontal direction. Same conventions as for velocities
 # Note:
 
+Calculates geostrophic velocities.
+Works with one or two horizontal dimensions and additional (time) dimensions.
+Dimensions are supposed to be ordered horizontal, vertical, other dimensions
+You must either provide the index for the level of no motion or ssh eta. NOTE THAT THE LEVEL IS AN INDEX NUMBER FOR THE MOMENT
+Dimensions of ssh must be the same as rhop in which vertical dimension has been taken out
+If you force fillin=false, then you must have created the density array without missing values outside of this call, as well as ssh if you provide it.
 
 """
 
