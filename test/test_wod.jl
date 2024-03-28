5aee3c7e487a93ab1b961d3b40097b64c00bb0a4
Alexander-Barth <barth.alexander@gmail.com>
2024-03-28 12:46:26 +0100

    use WorldOceanDatabase.extracttar

--- test/test_wod.jl
+++ test/test_wod.jl
@@ -1,24 +1,12 @@
-
-using ZipFile
 using Downloads: download
 using PhysOcean: WorldOceanDatabase
 using Test
 
-tmpfname = download("https://dox.uliege.be/index.php/s/QBFPUJvOMGOlOvS/download")
+
+tarname = download("https://dox.uliege.be/index.php/s/2QLL1g6DepKc0TT/download")
 woddir = tempname()
 mkdir(woddir)
-
-zarchive = ZipFile.Reader(tmpfname)
-for f in zarchive.files
-    fullpath = joinpath(woddir,f.name)
-    if (endswith(f.name,"/") || endswith(f.name,"\\"))
-        mkdir(fullpath)
-    else
-        write(fullpath, read(f))
-    end
-end
-close(zarchive)
-
+WorldOceanDatabase.extracttar(tarname,woddir)
 
 obsvalue,obslon,obslat,obsdepth,obstime,obsid = WorldOceanDatabase.load(
     Float64,woddir,"Temperature"; prefixid = "1977-")
